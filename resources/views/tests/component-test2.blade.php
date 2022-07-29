b48d00bb054f7520972a55eacbcebb8638855928
engineer_tacky <tsubasa801@gmail.com>
2022-07-29 21:22:47 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,5 +1,8 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー2</x-slot>
     コンポーネントテスト2
-    <x-tests.card title="タイトル2" content="コンテンツ2" :message="$message" />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージです" />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています" />
 </x-tests.app>
